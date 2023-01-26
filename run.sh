3386cb4e16d42b428e8c85b599609887f9aa178c
Ian McFarlane <i.mcfarlane2002@gmail.com>
2023-01-26 11:37:00 -0500

    use my nvim-remote script built with nix

--- run.sh
+++ run.sh
@@ -1,14 +1,5 @@
-#!/bin/bash
+#!/bin/sh
 
-# replace with your terminal
-term_exec=/bin/kitty
+echo "running with args ${file_arg}" >> $log
 
-server_path=$HOME/.cache/nvim/com.unity.server.pipe
-
-if [ -e $server_path ]; then
-    # open file in server
-    $term_exec -e nvim --server $server_path --remote "$@"
-else
-    # start the server if its pipe doesn't exist
-    $term_exec -e nvim --listen $server_path "$@"
-fi
+nvim-remote "$@"
