7bb7897f33a892ae4b06de51e62f95924302c162
Ian McFarlane <i.mcfarlane2002@gmail.com>
2023-09-21 18:38:28 -0400

    let nvim-remote know if its unity at all, not just flatpak unity

--- run.sh
+++ run.sh
@@ -14,5 +14,5 @@ if [ "${FLATPAK_SANDBOX_DIR#*$flatpak_substring}" ]; then
     # you need to run "sudo flatpak override com.unity.UnityHub --talk-name=org.freedesktop.Flatpak" for this to work
     flatpak-spawn --host $script_dir/nvim-remote "__UNITY_FLATPAK__ $file_arg" >> $log 2>&1
 else
-    $script_dir/nvim-remote "$file_arg"
+    $script_dir/nvim-remote "__UNITY__ $file_arg"
 fi
