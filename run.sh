bab84113d26bd7f85a217c3cc969867ac323e38d
Ian McFarlane <i.mcfarlane2002@gmail.com>
2023-09-21 17:13:13 -0400

    do processing on CLI args from unity for both flatpak and native install

--- run.sh
+++ run.sh
@@ -7,11 +7,12 @@ flatpak_substring="com.unity.UnityHub"
 # the classic stackoverflow "how get directory of script??"
 script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+# remove the weird thing that precedes filenames
+file_arg=$(echo "$@" | sed -e "s/+normal -1G-1| //g")
+
 if [ "${FLATPAK_SANDBOX_DIR#*$flatpak_substring}" ]; then
-    # remove the weird thing that precedes filenames
-    file_arg=$(echo "$@" | sed -e "s/+normal -1G-1| //g")
     # you need to run "sudo flatpak override com.unity.UnityHub --talk-name=org.freedesktop.Flatpak" for this to work
     flatpak-spawn --host $script_dir/nvim-remote "__UNITY_FLATPAK__ $file_arg" >> $log 2>&1
 else
-    $script_dir/nvim-remote "$@"
+    $script_dir/nvim-remote "$file_arg"
 fi
